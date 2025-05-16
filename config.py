9d8f8a9549f9424b1433e922a05a061db1ded674
quocanh <anhbqtq@gmail.com>
2025-05-17 04:44:43 +0700

    todo: change config.py later

--- config.py
+++ config.py
@@ -8,15 +8,13 @@ from motor.motor_asyncio import AsyncIOMotorClient
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Kết nối MongoDB
-MONGODB_URL = os.getenv("MONGODB")
+# MONGODB_URL = os.getenv("MONGODB")
+MONGODB_URL = "mongodb+srv://kiritosamsung:ngQte96hSvW6WiI2@cluster0.xi8yzg4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 _client = AsyncIOMotorClient(
     MONGODB_URL,
     tls=True,
-    tlsCAFile=certifi.where(),
-    serverSelectionTimeoutMS=5000,
-    socketTimeoutMS=None,
-    connectTimeoutMS=10000,
-    tlsAllowInvalidCertificates=False,
+    tlsCAFile=certifi.where()
 )
+
 db = _client["hotel_database"]
