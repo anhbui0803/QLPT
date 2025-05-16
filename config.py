747c107af7703e2877e7b17dd75ac83b0cd2595c
quocanh <anhbqtq@gmail.com>
2025-05-17 02:32:54 +0700

    change db connection

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # Lấy SECRET_KEY từ env hoặc dùng default
-SECRET_KEY = os.getenv("SESSION_SECRET", "dev-secret-do-not-use")
+SECRET_KEY = os.getenv("SESSION_SECRET")
 
 # Kết nối MongoDB
-MONGODB_URL = os.getenv("MONGODB", "mongodb://localhost:27017")
+MONGODB_URL = os.getenv("MONGODB")
 _client = AsyncIOMotorClient(MONGODB_URL)
 db = _client["hotel_database"]
